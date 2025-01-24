2ecf9e6f7f9833a318b07728db7ddb6dcfd04659
caizhihao <caizhihao@163.com>
2025-01-25 00:35:57 +0800

    feat(New): support rename @FastRetry anno

--- fast-retry-spring/src/main/java/com/burukeyou/retry/spring/core/FastRetryOperationsInterceptor.java
+++ fast-retry-spring/src/main/java/com/burukeyou/retry/spring/core/FastRetryOperationsInterceptor.java
@@ -2,6 +2,8 @@ package com.burukeyou.retry.spring.core;
 
 import com.burukeyou.retry.core.RetryQueue;
 import com.burukeyou.retry.core.task.RetryTask;
+import com.burukeyou.retry.spring.annotations.FastRetry;
+import com.burukeyou.retry.spring.utils.BizUtil;
 import lombok.Setter;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
@@ -9,7 +11,9 @@ import org.springframework.beans.factory.ListableBeanFactory;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -22,10 +26,18 @@ public class FastRetryOperationsInterceptor  implements MethodInterceptor {
 
     private AnnotationRetryTaskFactory<Annotation> annotationRetryTaskFactory;
 
+    private static final Map<Class<?>,Boolean> annotationRetryTaskFactoryMap = new ConcurrentHashMap<>();
+
     public FastRetryOperationsInterceptor(ListableBeanFactory beanFactory, RetryAnnotationMeta retryAnnotation) {
         this.beanFactory = beanFactory;
         this.retryAnnotation = retryAnnotation;
         annotationRetryTaskFactory = getRetryTaskFactory(retryAnnotation);
+
+        Class<?> retryTaskFactoryClass = annotationRetryTaskFactory.getClass();
+        if (!annotationRetryTaskFactoryMap.containsKey(retryTaskFactoryClass)){
+            Class<?> superClassParamFirstClass = BizUtil.getSuperClassParamFirstClass(retryTaskFactoryClass, AnnotationRetryTaskFactory.class);
+            annotationRetryTaskFactoryMap.put(retryTaskFactoryClass, FastRetry.class  == superClassParamFirstClass);
+        }
     }
 
     private AnnotationRetryTaskFactory<Annotation> getRetryTaskFactory(RetryAnnotationMeta retryAnnotation) {
@@ -36,7 +48,9 @@ public class FastRetryOperationsInterceptor  implements MethodInterceptor {
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Method method = invocation.getMethod();
         Class<?> returnType = method.getReturnType();
-        RetryTask<Object> retryTask = annotationRetryTaskFactory.getRetryTask(invocation, retryAnnotation.getSubAnnotation());
+
+        Boolean isFastRetryAnno = annotationRetryTaskFactoryMap.get(annotationRetryTaskFactory.getClass());
+        RetryTask<Object> retryTask = annotationRetryTaskFactory.getRetryTask(invocation, isFastRetryAnno ? retryAnnotation.getFastRetry() : retryAnnotation.getSubAnnotation());
         if (retryTask == null){
             return invocation.proceed();
         }
